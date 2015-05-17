aad9af06b25edcc00593f5568f5c975c77cd04c4
RX14 <chris@rx14.co.uk>
2015-05-17 18:29:11 +0100

    So I never even saved the response lol...

--- src/main/groovy/uk/co/rx14/jlaunchlib/caches/EtagCache.groovy
+++ src/main/groovy/uk/co/rx14/jlaunchlib/caches/EtagCache.groovy
@@ -13,12 +13,17 @@ class EtagCache extends Cache {
 	Path storage
 
 	String getLocalEtag(URL URL) {
-		def etagFile = getFilenames(URL)[1].toFile()
+		def etagFile = getEtagPath(URL).toFile()
 
 		etagFile.exists() ? etagFile.text : null
 	}
 
 	String get(URL URL) {
+		getPath(URL).toFile().parentFile.mkdirs()
+
+		if (getEtagPath(URL).toFile().exists() && !getPath(URL).toFile().exists())
+			getEtagPath(URL).toFile().delete()
+
 		def localEtag = getLocalEtag(URL)
 		def request = Unirest.get(URL.toString())
 
@@ -31,18 +36,23 @@ class EtagCache extends Cache {
 		println "$URL: $response.status"
 
 		if (response.status == 304) { //Return from cache
-			getFilenames(URL)[0].text
+			getPath(URL).text
 		} else if (response.status == 200) { //Return from response
+			getEtagPath(URL).text = response.headers.getFirst("etag")
+			getPath(URL).text = response.body
 			response.body
 		} else {
 			throw new HTTPException(response.status)
 		}
 	}
 
-	private List<Path> getFilenames(URL URL) {
+	private Path getPath(URL URL) {
 		if (URL.file.endsWith("/")) return null
+		storage.resolve(URL.path.substring(URL.path.lastIndexOf("/") + 1))
+	}
 
-		def file = storage.resolve(URL.path.substring(URL.path.lastIndexOf("/") + 1))
-		[storage.resolve(file), storage.resolve("${file}.etag")]
+	private Path getEtagPath(URL URL) {
+		if (URL.file.endsWith("/")) return null
+		new File("${getPath(URL)}.etag").toPath()
 	}
 }
