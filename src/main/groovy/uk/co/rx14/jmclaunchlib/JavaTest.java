f76676ef31c0730be502f993a70ec0cafebca18e
RX14 <chris@rx14.co.uk>
2015-07-27 11:04:16 +0100

    Java Test

--- src/main/groovy/uk/co/rx14/jmclaunchlib/JavaTest.java
+++ src/main/groovy/uk/co/rx14/jmclaunchlib/JavaTest.java
@@ -1,4 +1,34 @@
 package uk.co.rx14.jmclaunchlib;
 
+import uk.co.rx14.jmclaunchlib.util.ChangePrinter;
+
+import java.io.*;
+
 public class JavaTest {
+	public static void main(String[] args) {
+		LaunchTask task = new LaunchTaskBuilder()
+			.setCachesDir("test/caches")
+			.setMinecraftVersion("1.7.10")
+			.setForgeVersion()
+			.setInstanceDir("test/instance")
+			.setUsername("RX14")
+			.setOffline()
+			.build();
+
+		new ChangePrinter(() -> "" + task.getCompletedPercentage(), 100).start();
+
+		LaunchSpec spec = task.getSpec();
+
+		Process run = spec.run(new File("/usr/bin/java").toPath());
+
+		BufferedReader stdout = new BufferedReader(new InputStreamReader(run.getInputStream()));
+		String line = null;
+		try {
+			while ((line = stdout.readLine()) != null) {
+				System.out.println(line);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
